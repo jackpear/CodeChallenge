81c95f8ec31a28140266914ac9341b39d61e4353
jackp <jackpearson784@gmail.com>
2024-08-10 14:39:44 -0500

    Added invisible (turqiose for now) gap divs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,22 +40,39 @@ module ApplicationHelper
     (time.hour*60 + time.min - 360)*2
   end
 
-  def calculate_gaps(time_slots)
+  def times_to_minutes(time1, time2)
+    (time2 - time1)/60 # This returns how many minutes are between the times
+  end
+
+  def get_end_time(time, duration,pxScale)
+    time + duration*(60/pxScale)
+  end
+
+  def calculate_gaps(time_slots,pxScale)
     gaps = []
     
     # Ensure the slots are sorted by start time
     sorted_slots = time_slots.sort_by { |start_time, _,_,_| start_time }
-    
+    # puts "!!!!!!!!!!!!!!!!!!!! #{times_to_minutes(Time.parse("2000-01-01 0:00"),sorted_slots[0][0])}"
+    newGap = [Time.parse("2000-01-01 6:00"),times_to_minutes(Time.parse("2000-01-01 0:00"),sorted_slots[0][0])*pxScale]
+    if (newGap[1] > 0)
+      gaps << newGap # adds first gap
+    end
+    end_time = sorted_slots.last[0] + sorted_slots.last[1]*30
+    newGap = [end_time, times_to_minutes(end_time,Time.parse("2000-01-01 10:00"))*pxScale]
+    if (newGap[1] > 0)
+      gaps << newGap # adds last gap
+    end
     # Iterate through the list to find gaps
     sorted_slots.each_cons(2) do |(start_time1, duration1,_,_), (start_time2, _,_,_)|
       duration1 = duration1/2
       end_time1 = start_time1 + duration1 * 60  # Calculate the end time of the first slot
       if start_time2 > end_time1
-        gap_duration = (start_time2 - end_time1) / 60  # Convert gap from seconds to minutes
-        gaps << [end_time1, gap_duration.to_i]  # Add the gap as a tuple
+        gap_duration = times_to_minutes(end_time1,start_time2)  # Convert gap from seconds to minutes
+        gaps << [end_time1, gap_duration.to_i*pxScale]  # Add the gap as a tuple
       end
     end
-    
+    puts "  "
     gaps
   end
   
