a9b3e992efcd7dbb4b7b74209e72e11efc9498f3
jackp <jackpearson784@gmail.com>
2024-08-11 17:42:59 -0500

    Cleaned up pxScale (removed it)

--- app/controllers/technicians_controller.rb
+++ app/controllers/technicians_controller.rb
@@ -1,7 +1,7 @@
 class TechniciansController < ApplicationController
   def index
     @technicians = Technician.all
-    @orders = Order.all 
+    @orders = Order.all
     @locations = Location.all
     @times = ["6:00","7:00","8:00", "9:00","10:00","11:00","12:00","13:00","14:00","15:00"]
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,19 +20,13 @@ module ApplicationHelper
     tuples
   end
 
-  def orders_to_tuples(orders,pxScale)
+  def orders_to_tuples(orders)
     tuples = orders.map do |order|
       start_time = order.time 
       duration = order.duration 
-      [start_time,duration*pxScale,order.location_id, order.price]   # I always use pxScale of 2 to make an hour slot 120px tall
+      [start_time,duration,order.location_id, order.price]
     end
     tuples
-  end
-
-  def tuples_to_betweens(tuples,pxScale)
-    sorted = tuples.sort_by { |start_time,_,_,_| start_time } # sorts orders
-
-    
   end
 
   def time_to_pxOffset(time)
@@ -44,32 +38,31 @@ module ApplicationHelper
     (time2 - time1)/60 # This returns how many minutes are between the times
   end
 
-  def get_end_time(time, duration,pxScale)
-    time + duration*(60/pxScale)
+  def get_end_time(time, duration)
+    time + duration*60
   end
 
-  def calculate_gaps(time_slots,pxScale)
+  def calculate_gaps(time_slots)
     gaps = []
     
     # Ensure the slots are sorted by start time
     sorted_slots = time_slots.sort_by { |start_time, _,_,_| start_time }
-    # puts "!!!!!!!!!!!!!!!!!!!! #{times_to_minutes(Time.parse("2000-01-01 0:00"),sorted_slots[0][0])}"
-    newGap = [Time.parse("2000-01-01 6:00"),times_to_minutes(Time.parse("2000-01-01 0:00"),sorted_slots[0][0])*pxScale]
+    newGap = [Time.parse("2000-01-01 6:00"),times_to_minutes(Time.parse("2000-01-01 0:00"),sorted_slots[0][0])]
     if (newGap[1] > 0)
       gaps << newGap # adds first gap
     end
-    end_time = sorted_slots.last[0] + sorted_slots.last[1]*30
-    newGap = [end_time, times_to_minutes(end_time,Time.parse("2000-01-01 10:00"))*pxScale]
+    end_time = sorted_slots.last[0] + sorted_slots.last[1]*60
+    newGap = [end_time, times_to_minutes(end_time,Time.parse("2000-01-01 10:00"))]
     if (newGap[1] > 0)
       gaps << newGap # adds last gap
     end
-    # Iterate through the list to find gaps
+
     sorted_slots.each_cons(2) do |(start_time1, duration1,_,_), (start_time2, _,_,_)|
-      duration1 = duration1/2
+      duration1 = duration1
       end_time1 = start_time1 + duration1 * 60  # Calculate the end time of the first slot
       if start_time2 > end_time1
         gap_duration = times_to_minutes(end_time1,start_time2)  # Convert gap from seconds to minutes
-        gaps << [end_time1, gap_duration.to_i*pxScale]  # Add the gap as a tuple
+        gaps << [end_time1, gap_duration.to_i]  # Add the gap as a tuple
       end
     end
     puts "  "
