1dc7ce6a32cb56ad381960af540f0df9af3d9fbc
jackp <jackpearson784@gmail.com>
2024-08-11 17:45:12 -0500

    Added root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/technicians", to: "technicians#index"
+  root "technicians#index"
+  
+  get "/schedule", to: "technicians#index"
   # Defines the root path route ("/")
   # root "posts#index"
 end
